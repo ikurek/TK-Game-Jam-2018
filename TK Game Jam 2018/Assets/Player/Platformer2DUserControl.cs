b3c469cdf25d452b47f109071de4bce62a29a0fe
Gabriele Picco <piccogabriele@gmail.com>
2018-03-04 10:03:15 +0100

    Disable initial input

--- TK Game Jam 2018/Assets/Player/Platformer2DUserControl.cs
+++ TK Game Jam 2018/Assets/Player/Platformer2DUserControl.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
@@ -7,8 +8,12 @@ namespace UnityStandardAssets._2D
     [RequireComponent(typeof (PlatformerCharacter2D))]
     public class Platformer2DUserControl : MonoBehaviour
     {
+        public float startInputDelay = 3f;
+
         private PlatformerCharacter2D m_Character;
         private bool m_Jump;
+
+        private bool inputEnbled = false;
 
 
         private void Awake()
@@ -16,25 +21,42 @@ namespace UnityStandardAssets._2D
             m_Character = GetComponent<PlatformerCharacter2D>();
         }
 
+        private void Start()
+        {
+            StartCoroutine(EnableInput());
+        }
 
         private void Update()
         {
-            if (!m_Jump)
+            if (inputEnbled)
             {
-                // Read the jump input in Update so button presses aren't missed.
-                m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+                if (!m_Jump)
+                {
+                    // Read the jump input in Update so button presses aren't missed.
+                    m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+                }
             }
         }
 
 
         private void FixedUpdate()
         {
-            // Read the inputs.
-            bool crouch = Input.GetKey(KeyCode.LeftControl);
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
-            // Pass all parameters to the character control script.
-            m_Character.Move(h, crouch, m_Jump);
-            m_Jump = false;
+            if (inputEnbled)
+            {
+                // Read the inputs.
+                bool crouch = Input.GetKey(KeyCode.LeftControl);
+                float h = CrossPlatformInputManager.GetAxis("Horizontal");
+                // Pass all parameters to the character control script.
+                m_Character.Move(h, crouch, m_Jump);
+                m_Jump = false;
+            }
         }
+
+        private IEnumerator EnableInput()
+        {
+            yield return new WaitForSeconds(startInputDelay);
+            inputEnbled = true;
+        }
+
     }
 }
