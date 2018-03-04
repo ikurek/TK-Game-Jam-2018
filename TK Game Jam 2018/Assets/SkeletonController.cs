c266c8a89041eb2ec241612d3be0c6dec2286647
Igor Kurek <igorkurek96@gmail.com>
2018-03-04 03:19:58 +0100

    Fixed Level 2, added blood effects

--- TK Game Jam 2018/Assets/SkeletonController.cs
+++ TK Game Jam 2018/Assets/SkeletonController.cs
@@ -14,4 +14,11 @@ public class SkeletonController : MonoBehaviour {
 		
 	}
 
+	void OnCollisionEnter2D(Collision2D collision) {
+		if(collision.collider.tag == "Player") {
+			Debug.Log("Test");
+			gameObject.GetComponentInChildren<ParticleSystem>().Play();
+		}
+	}
+
 }
