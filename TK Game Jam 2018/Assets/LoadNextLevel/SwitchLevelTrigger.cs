01c20547d299870c025fcb17fdcd2dec73c3360a
Gabriele Picco <piccogabriele@gmail.com>
2018-03-04 01:48:45 +0100

    NextLevelTransition

---
+++ TK Game Jam 2018/Assets/LoadNextLevel/SwitchLevelTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using EZCameraShake;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchLevelTrigger : MonoBehaviour
+{
+
+	private void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Player")
+		{
+            float fadeTime = GameObject.Find("_GM").GetComponent<Fading>().BeginFade(1);
+            StartCoroutine(LoadNextScene(fadeTime));
+		}
+	}
+
+	IEnumerator LoadNextScene(float fadeTime)
+	{
+    yield return new WaitForSeconds(fadeTime);
+		int index = SceneManager.GetActiveScene().buildIndex;
+		SceneManager.LoadScene(index + 1);
+	}
+}
